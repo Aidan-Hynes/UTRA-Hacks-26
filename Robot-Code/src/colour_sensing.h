f28424b59b35caa749c8298d97e345ddd1ced937
DinoCode15 <eliashynes2@gmail.com>
2026-01-31 17:39:30 -0500

    Added colour sensing, red, blue, green, black, and white.

---
+++ Robot-Code/src/colour_sensing.h
@@ -0,0 +1,7 @@
+#include <Arduino.h>
+#include <MD_TCS230.h>
+#include <FreqCount.h>
+
+void readSensor();
+extern MD_TCS230 colourSensor;
+extern colorData colour;
--- Robot-Code/src/main.cpp
+++ Robot-Code/src/main.cpp
@@ -1,14 +1,32 @@
 #include <Arduino.h>
 #include <HCSR04.h>
+#include <MD_TCS230.h>
+#include <FreqCount.h>
+#include "colour_sensing.h"
 
-UltraSonicDistanceSensor distanceSensor(9, 10);  // trigger, echo
+#define S0 4
+#define S1 8
+#define S2 6
+#define S3 7
+#define OE 3    // LOW = ENABLED 
+#define TCS_OUT 5 // TCS230 OUT must be on pin 5 for FreqCount on UNO
+
+//UltraSonicDistanceSensor distanceSensor(9, 10);  // trigger, echo
 
 void setup() {
     Serial.begin(9600);
+    Serial.println("[TCS230 Simple BLOCKING Example]");
+    
+    FreqCount.begin(1000);
+    
+    colourSensor.begin();
+    colourSensor.setEnable(true);
+    colourSensor.setFilter(TCS230_RGB_X);
+    colourSensor.setFrequency(TCS230_FREQ_HI);
+    colourSensor.setSampling(10);
 }
 
 void loop() {
-    long distance = distanceSensor.measureDistanceCm();
-    Serial.println(distance);
-    delay(10);
+    readSensor();
+    delay(1000);
 }
