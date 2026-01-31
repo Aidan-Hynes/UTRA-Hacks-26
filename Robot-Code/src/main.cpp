cc7493a6a6b8d32a4ca002ed1402bf71759cf231
Aidan <pianomanaidan@gmail.com>
2026-01-31 17:04:48 -0500

    Add custom motor library

--- Robot-Code/src/main.cpp
+++ Robot-Code/src/main.cpp
@@ -1,14 +1,47 @@
 #include <Arduino.h>
-#include <HCSR04.h>
+#include <Motor.h>
 
-UltraSonicDistanceSensor distanceSensor(9, 10);  // trigger, echo
+// Definitions Arduino pins connected to input H Bridge
+int IN1 = 4;
+int IN2 = 5;
+int IN3 = 6;
+int IN4 = 7;
+int speed = 255; // Speed value between 0 and 255
 
-void setup() {
+Motor motor(IN1, IN2, IN3, IN4); // Create motor object
+
+void setup()
+{
     Serial.begin(9600);
 }
 
-void loop() {
-    long distance = distanceSensor.measureDistanceCm();
-    Serial.println(distance);
-    delay(100);
+void loop()
+{
+    String readString = "";
+  // Check if any data is available to read from the serial port
+    while (Serial.available()) {
+        // Read characters into a String until the buffer is empty
+        char c = Serial.read();
+        readString += c;
+        delay(2); // Small delay to allow the buffer to fill
+    }
+    if (readString.length() == 0) {
+        return; // No data read
+    }
+    Serial.println("Received: " + readString);
+    if (readString == "S") {
+        motor.stop(); // Stop the motor
+    }
+    else if (readString == "F") {
+        // Move Forward
+        motor.drive(true, speed);
+    }
+    else if (readString == "B") {
+        // Move Backward
+        motor.drive(false, speed);
+    }
+    else if ((readString != String(speed)) && (readString.toInt() >= 0) && (readString.toInt() <= 255)) {
+        // Update speed value
+        speed = readString.toInt();
+    }
 }
