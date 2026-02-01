65fa17241c42f0e6b488d956520e7b0e3b05a75e
Aidan <pianomanaidan@gmail.com>
2026-02-01 10:25:05 -0500

    Upload state machine

--- Robot-Code/src/main.cpp
+++ Robot-Code/src/main.cpp
@@ -29,12 +29,17 @@ const int motor2Pin2 = 10;
 
 typedef enum {
     START_PHASE,
+    TARGET_COURSE,
+    TARGET_RAMP,
+    TARGET_TRAVERSE,
+    TARGET_FIND_BLACK,
+    TARGET_LEAVE,
+    TARGET_END,
     OBSTACLE_COURSE,
     OBSTACLE_COURSE_W_BOX,
     GOING_AROUND_OBSTACLE,
+    PICKING_UP_BOX,
     DROPPING_OFF_BOX,
-    TARGET_COURSE,
-    PICKING_UP_BOX
 } state;
 
 static ColourSensing colourSensor(s0, s1, s2, s3, ColourOutPin);
@@ -42,6 +47,8 @@ static Motor motors(motor1Pin1, motor1Pin2, motor2Pin1, motor2Pin2);
 static SRF05 ultra(ultrasonicTriggerPin, ultrasonicEchoPin);
 static state currState;
 static Servo forkServo;
+
+extern void linefollow(Motor* motor, ColourSensing* colourSensor, ColourSensing::DetectedColour lineColour, bool rightSide, float gain);
 
 
 ColourSensing::rgb getAverageColour(int samples) {
@@ -68,12 +75,6 @@ int getAverageDistance(int samples) {
         delay(20); // Small delay between readings
     }
     return totalDistance / samples;
-}
-
-void pickUpBox() {
-    // Lower fork
-    // Move forward
-    // Raise fork
 }
 
 void goAroundObstacle() {
@@ -135,6 +136,35 @@ void dropObstacleBox() {
     motors.turnLeft90();
 }
 
+void pickUpTargetBox() {
+    motors.turnRight90();
+    lowerFork();
+    motors.drive(false, 100);
+    delay(2000);
+    motors.stop();
+    raiseFork();
+    delay(500);
+    motors.drive(true, 100);
+    delay(2000);
+    motors.stop();
+    motors.turnLeft90();
+    motors.turnLeft90();
+    motors.drive(false, 100);
+    delay(1000);
+    motors.stop();
+    lowerFork();
+    delay(500);
+    motors.drive(true, 100);
+    delay(1000);
+    motors.stop();
+    motors.turnRight90();
+    motors.drive(false, 255);
+    delay(500);
+}
+
+
+
+
 void setup() {
     Serial.begin(9600);
     unsigned long startTime = millis();
@@ -155,9 +185,98 @@ void loop() {
     switch (currState)
     {
     case START_PHASE:
-        motors.drive(false, 255);
-        delay(3000); // Move forward for 3 seconds
-        currState = OBSTACLE_COURSE;
+        linefollow(&motors, &colourSensor, ColourSensing::COLOUR_BLACK, true, 1.0f);
+        if(colourSensor.getClosestColour(colour, dist) == ColourSensing::COLOUR_GREEN) {
+            Serial.println("Branch detected, starting ramp/target course!");
+            motors.stop();
+            delay(500);
+            currState = TARGET_COURSE;
+        }
+        break;
+    case TARGET_COURSE:
+        linefollow(&motors, &colourSensor, ColourSensing::COLOUR_GREEN, true, 1.0f);
+        if (colourSensor.getClosestColour(colour, dist) == ColourSensing::COLOUR_BLUE) {
+            Serial.println("Target box detected!");
+            motors.stop();
+            pickUpTargetBox();
+            break;
+        }
+        if (colourSensor.getClosestColour(colour, dist) == ColourSensing::COLOUR_BLACK) {
+            Serial.println("Ramp detected, moving up!");
+            motors.stop();
+            delay(500);
+            currState = TARGET_RAMP;
+            break;
+        }
+        break;
+    case TARGET_RAMP:
+        linefollow(&motors, &colourSensor, ColourSensing::COLOUR_BLACK, true, 1.0f);
+        if (colourSensor.getClosestColour(colour, dist) == ColourSensing::COLOUR_BLUE) {
+            Serial.println("Ramp ended, stopping!");
+            motors.stop();
+            delay(500);
+            currState = TARGET_TRAVERSE;
+            break;
+        }
+        break;
+    case TARGET_TRAVERSE:
+        motors.drive(false, 100);
+        if (colourSensor.getClosestColour(colour, dist) == ColourSensing::COLOUR_RED) {
+            motors.stop();
+            delay(500);
+            motors.turnLeft90();
+            ColourSensing::rgb left_colour = getAverageColour(5);
+            motors.turnRight90();
+            motors.turnRight90();
+            ColourSensing::rgb right_colour = getAverageColour(5);
+            
+            if (colourSensor.getClosestColour(left_colour, dist) == ColourSensing::COLOUR_RED
+            && colourSensor.getClosestColour(right_colour, dist) == ColourSensing::COLOUR_GREEN) {
+                motors.drive(false, 100);
+                currState = TARGET_FIND_BLACK;
+            } else if (colourSensor.getClosestColour(left_colour, dist) == ColourSensing::COLOUR_RED
+            && colourSensor.getClosestColour(right_colour, dist) == ColourSensing::COLOUR_BLACK) {
+                motors.drive(false, 100);
+                currState = TARGET_FIND_BLACK;
+            } else if (colourSensor.getClosestColour(left_colour, dist) == ColourSensing::COLOUR_GREEN
+            && colourSensor.getClosestColour(right_colour, dist) == ColourSensing::COLOUR_RED) {
+                motors.turnLeft90();
+                motors.turnLeft90();
+                motors.drive(false, 100);
+                currState = TARGET_FIND_BLACK;
+            } else if (colourSensor.getClosestColour(left_colour, dist) == ColourSensing::COLOUR_BLACK
+            && colourSensor.getClosestColour(right_colour, dist) == ColourSensing::COLOUR_RED) {
+                motors.turnLeft90();
+                motors.turnLeft90();
+                motors.drive(false, 100);
+                currState = TARGET_FIND_BLACK;
+            }
+            else {
+                motors.drive(false, 100);
+            }
+            currState = OBSTACLE_COURSE;
+        }
+        break;
+    case TARGET_FIND_BLACK:
+        if (colourSensor.getClosestColour(colour, dist) == ColourSensing::COLOUR_BLACK) {
+            motors.stop();
+        }
+        break;
+    case TARGET_LEAVE:
+        linefollow(&motors, &colourSensor, ColourSensing::COLOUR_BLACK, true, 1.0f);
+        if (colourSensor.getClosestColour(colour, dist) == ColourSensing::COLOUR_GREEN) {
+            Serial.println("Leaving target area, heading to obstacle course!");
+            currState = TARGET_END;
+        }
+        break;
+    case TARGET_END:
+        linefollow(&motors, &colourSensor, ColourSensing::COLOUR_GREEN, true, 1.0f);
+        if (colourSensor.getClosestColour(colour, dist) == ColourSensing::COLOUR_RED) {
+            Serial.println("Rejoined main course, heading to obstacle course!");
+            motors.stop();
+            delay(500);
+            currState = OBSTACLE_COURSE;
+        }
         break;
     case OBSTACLE_COURSE:
         if (colourSensor.getClosestColour(colour, dist) == ColourSensing::COLOUR_BLUE) {
